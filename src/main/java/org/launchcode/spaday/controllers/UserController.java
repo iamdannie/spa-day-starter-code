18584f1c8572feeaf20b8c536eb0e13bce1e8901
iamdannie <iamtaiyco@gmail.com>
2021-02-27 11:22:36 -0600

    add and check for errors by adding user in the model and adding changes to add.html

--- src/main/java/org/launchcode/spaday/controllers/UserController.java
+++ src/main/java/org/launchcode/spaday/controllers/UserController.java
@@ -3,7 +3,10 @@ package org.launchcode.spaday.controllers;
 import org.launchcode.spaday.models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("user")
@@ -11,23 +14,24 @@ public class UserController {
 
     @GetMapping("/add")
     public String displayAddUserForm(Model model) {
-        model.addAttribute(new User()),
+        model.addAttribute(new User());
         return "user/add";
     }
 
     @PostMapping
-    public String processAddUserForm(Model model, @ModelAttribute User user, String verify) {
+    public String processAddUserForm(Model model, @ModelAttribute @Valid User user, Errors errors, String verify) {
         model.addAttribute("user", user);
         model.addAttribute("verify", verify);
-        model.addAttribute("username", user.getUsername());
-        model.addAttribute("email", user.getEmail());
 
-        if (user.getPassword().equals(verify)) {
-           return "user/index";
+
+        if (!user.getPassword().equals(verify) || errors.hasErrors()) {
+            model.addAttribute("error", "There is an error");
+                       return "user/add";
+
         }
         else {
-            model.addAttribute("error", "Passwords do not match");
-            return "user/add";
+
+            return "user/index";
         }
 
     }
